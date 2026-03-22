94eabce4129c143e620e13faeab7672631dcfcbc
farha <farha.ahmed1@ucalgary.ca>
2026-03-22 04:45:44 -0600

    Restore quiz scene and timer to stable working version

--- Assets/QuizManager.cs
+++ Assets/QuizManager.cs
@@ -7,13 +7,20 @@ using UnityEngine.SceneManagement;
 public class QuizManager : MonoBehaviour
 {
     public List<QuestionAndAnswers> QnA;
-    public GameObject[] options; //button
+    public GameObject[] options;
     public int currentQuestion;
 
     public GameObject StartPanel;
     public GameObject InfoPanel;
     public GameObject QuizPanel;
     public GameObject GameOverPanel;
+    public GameObject TimerUI;
+
+    public Image timerFill;
+    public float timePerQuestion = 45f;
+
+    private float currentTime;
+    private bool timerRunning = false;
 
     public Text QuestionText;
     public Text ScoreText;
@@ -25,11 +32,12 @@ public class QuizManager : MonoBehaviour
     private void Start()
     {
         TotalQuestions = Mathf.Min(5, QnA.Count);
-        
+
         StartPanel.SetActive(true);
         InfoPanel.SetActive(false);
         QuizPanel.SetActive(false);
         GameOverPanel.SetActive(false);
+        TimerUI.SetActive(false);
     }
 
     public void ShowInfo()
@@ -47,6 +55,7 @@ public class QuizManager : MonoBehaviour
         StartPanel.SetActive(false);
         InfoPanel.SetActive(false);
         QuizPanel.SetActive(true);
+        TimerUI.SetActive(true);
         generateQuestion();
     }
 
@@ -57,6 +66,9 @@ public class QuizManager : MonoBehaviour
 
     public void GameOver()
     {
+        timerRunning = false;
+        TimerUI.SetActive(false);
+
         QuizPanel.SetActive(false);
         GameOverPanel.SetActive(true);
         ScoreText.text = "Score: " + score + "/" + TotalQuestions;
@@ -64,6 +76,7 @@ public class QuizManager : MonoBehaviour
 
     public void correct()
     {
+        timerRunning = false;
         score += 1;
         questionsAsked += 1;
         QnA.RemoveAt(currentQuestion);
@@ -72,6 +85,7 @@ public class QuizManager : MonoBehaviour
 
     public void wrong()
     {
+        timerRunning = false;
         questionsAsked += 1;
         QnA.RemoveAt(currentQuestion);
         generateQuestion();
@@ -98,11 +112,33 @@ public class QuizManager : MonoBehaviour
             currentQuestion = Random.Range(0, QnA.Count);
             QuestionText.text = QnA[currentQuestion].Question;
             SetAnswers();
+
+            TimerUI.SetActive(true);
+            currentTime = timePerQuestion;
+            timerRunning = true;
+            timerFill.fillAmount = 1f;
         }
         else
         {
             Debug.Log("No Questions Left");
             GameOver();
+        }
+    }
+
+    private void Update()
+    {
+        if (timerRunning)
+        {
+            currentTime -= Time.deltaTime;
+            timerFill.fillAmount = currentTime / timePerQuestion;
+
+            if (currentTime <= 0f)
+            {
+                currentTime = 0f;
+                timerFill.fillAmount = 0f;
+                timerRunning = false;
+                wrong();
+            }
         }
     }
 }
