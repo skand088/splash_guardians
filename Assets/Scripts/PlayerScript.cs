7e822fd96b449aab1875d99d9c8c0fcf120178c3
noorsyed16 <noorsyed464@gmail.com>
2026-03-23 19:23:57 -0600

    Fixed score not updating issue in Alage and Trash game (#24)
    Fixed the score so it appears and updates during gameplay for the algae game and the trash game

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -61,7 +61,7 @@ namespace splash_guardians
         }
 
         //adding a function to detect collosion with the algae object and each collision causes score to increase and for the object to disappear
-        private void OnTriggerEnter2D(Collider2D algae_object)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             // Prevent some errors while in main map.
             if (HeldItem == null)
@@ -69,12 +69,21 @@ namespace splash_guardians
                 return;
             }
             //check if it an algae object
-            if (algae_object.CompareTag("Algae") && HeldItem.IsTouching(algae_object))
+            if (other.CompareTag("Algae") && HeldItem.IsTouching(other))
             {
                 AlgaeScore++; // increase algae score
-                Destroy(algae_object.gameObject);
+                Destroy(other.gameObject);
                 Debug.Log("Algae Score: " + AlgaeScore);
-                if (ScoreText != null) ScoreText.text = "Algae Score: " + AlgaeScore;
+                if (ScoreText != null) ScoreText.text = "Score: " + AlgaeScore;
+            }
+
+            //check if trash object
+            if (other.CompareTag("Trash") && HeldItem.IsTouching(other))
+            {
+                TrashScore++;
+                Destroy(other.gameObject);
+                Debug.Log("Trash Score: " + TrashScore);
+                if (ScoreText != null) ScoreText.text = "Score: " + TrashScore;
             }
         }
 
