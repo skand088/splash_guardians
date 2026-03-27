0882c35717370f081b9702e70ac09da11bfbfe3b
noorsyed16 <noorsyed464@gmail.com>
2026-03-26 22:35:31 -0600

    Adding responsive messages after game to algae game

--- Assets/Scripts/AlgaeGameOverScreenUI.cs
+++ Assets/Scripts/AlgaeGameOverScreenUI.cs
@@ -10,6 +10,7 @@ public class AlgaeGameOverScreenUI : MonoBehaviour
     public TMP_Text scoreOutputText;
     public ProgressService ProgressService;
     public string EmptyScoresText = "No scores yet.";
+    public TMP_Text scoreMessageText;
 
     private void Start()
     {
@@ -24,6 +25,21 @@ public class AlgaeGameOverScreenUI : MonoBehaviour
     private void OnEnable()
     {
         _ = RefreshScoreTextAsync();
+
+        //to display a message to the user based on how they performed
+        PlayerScript player = FindFirstObjectByType<PlayerScript>();
+        if (player != null && scoreMessageText != null)
+        {
+            int finalScore = player.AlgaeScore;
+
+            //base on users performance
+            if (finalScore < 15)
+                scoreMessageText.text = "Uh oh! You only scored " + finalScore + ", you didn't stop the algae!";
+            else if (finalScore <= 30)
+                scoreMessageText.text = "You scored " + finalScore + "! Try catching more algae next time.";
+            else
+                scoreMessageText.text = "You did it! You stopped all the algae and scored" + finalScore + "!";
+        }
     }
 
     private async Task RefreshScoreTextAsync()
